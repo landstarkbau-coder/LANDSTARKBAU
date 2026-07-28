fbccb3f0b365e07538774c05538c78b763c5e6c7
GregIvanchuk <your-email@example.com>
2026-07-28 12:09:14 +0300

    feat: add google analitics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-
+import Script from "next/script";
 import "@/app/globals.css";
 
 import { Header } from "./components/Header";
@@ -132,6 +132,18 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LPX6LEGMTD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LPX6LEGMTD');
+          `}
+        </Script>
         <ThemeProvider>
           <NextIntlClientProvider messages={messages}>
             <Header />
